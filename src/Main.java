56ecd25548763d5216c577f453952c18f0ae5c55
louvineaony <627991038@qq.com>
2019-04-14 11:43:06 +0800

    demo

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
  * Created by hyf on 2019/4/14 1:23
  */
 public class Main {
+
+    private static String url = "https://nocachemedia.ucampus.unipus.cn/uexercise/static/res/";
+
     public static void main(String[] args) {
         Fuck fuck = new Fuck();
 
@@ -13,8 +16,11 @@ public class Main {
 
 //        上线
         Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入答案网址");
-        fuck.fuck(scanner.nextLine());
+        System.out.println("请输入学校Id");
+        url = url + scanner.nextLine() + "/";
+        System.out.println("请输入试卷Id");
+        url = url + scanner.nextLine() + ".htm";
+        fuck.fuck(url);
         scanner.close();
 
         CreateFile createFile = new CreateFile();
